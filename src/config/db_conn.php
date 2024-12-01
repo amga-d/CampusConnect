855ebac28d246e81060c1bac4b6da6faad9190eb
amgad <amgad.amgad.abdulrazzaq@gmail.com>
2024-12-01 15:14:54 +0700

    update database name

--- src/config/db_conn.php
+++ src/config/db_conn.php
@@ -3,7 +3,7 @@
 $db_server = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "campusconnect1";
+$db_name = "campusconnect";
 
 function connect_db() {
     global $db_server, $db_user, $db_pass, $db_name;
